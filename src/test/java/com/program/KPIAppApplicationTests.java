9ff1dcfd9972e16283c4ce2f73303bf8efa0cde2
Adz4a <maratovadilzhan4@gmail.com>
2023-06-03 01:14:08 +0600

    remove test

--- src/test/java/com/program/KPIAppApplicationTests.java
+++ src/test/java/com/program/KPIAppApplicationTests.java
@@ -1,46 +1,10 @@
 package com.program;
 
-import com.program.model.User;
-import com.program.repository.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 
 import java.util.Optional;
 
 @SpringBootTest
 class KPIAppApplicationTests {
-
-	@Autowired
-	private UserRepository userRepository;
-
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-	@Autowired
-	public KPIAppApplicationTests(BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-
-	@Bean
-	public PasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Test
-	void contextLoads() {
-		User user = userRepository.findByEmail("azat@gmail.com");
-//		User newUser = user.get();
-		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-		userRepository.save(user);
-	}
-
-	@Test
-	void newTest(){
-	}
-
 
 }
